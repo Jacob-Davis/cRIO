b8573e29986fe8c01088771cb14cf840e02890f5
NuclearFej <jeffapm@gmail.com>
2014-02-04 14:46:32 -0500

    Added line to batch file

--- Connect to cRIO.bat
+++ Connect to cRIO.bat
@@ -1,2 +1,3 @@
 netsh interface ip set address name="Local Area Connection" static 10.16.72.5 255.0.0.0 10.16.72.4 1
 netsh interface ip set address name="Ethernet" static 10.16.72.5 255.0.0.0 10.16.72.4 1
+netsh interface ip set address name="Wi-Fi" static 10.16.72.5 255.0.0.0 10.16.72.4 1
