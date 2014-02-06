507b030ec92dfd3a1e6138fd84d513ba37ec5f64
NuclearFej <jeffapm@gmail.com>
2014-02-06 15:35:42 -0500

    Re-added project xml files

--- Connect to Robot WiFi.bat
+++ Connect to Robot WiFi.bat
@@ -1,1 +1,2 @@
-netsh interface ip set address name="Wi-Fi" static 10.16.72.5 255.0.0.0 10.16.72.4 1
+netsh interface ip set address name="Wi-Fi" static 10.16.72.2 255.0.0.0 10.16.72.4 1
+netsh interface ip set address name="Wireless Network Connection" static 10.16.72.2 255.0.0.0 10.16.72.4 1
