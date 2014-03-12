5b5fdeef0215d3d3b79297fb953c8aaba149b40b
InsanesTheName <accounts2952@gmail.com>
2014-03-12 15:25:44 -0400

    Not much

--- Connect to Internet.bat
+++ Connect to Internet.bat
@@ -1,3 +1,3 @@
-netsh interface ip set address "Local Area Connection" dhcp
-netsh interface ip set address "Ethernet" dhcp
-netsh interface ip set address "Wi-Fi" dhcp
+netsh interface ip set address "Local Area Connection" DHCP
+netsh interface ip set address "Ethernet" DHCP
+netsh interface ip set address "Wi-Fi" DHCP
